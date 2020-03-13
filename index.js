4e377a0dfe91e695794da1699729b993d7790a49
kenanhancer <kenanhancer@gmail.com>
2020-03-13 15:05:34 +0000

    nut-ioc provides interceptor feature for method calls

--- index.js
+++ index.js
@@ -5,18 +5,51 @@ const nutIocContainer = nutIoc();
 
 const mainAsync = async () => {
 
-    const ignoredDependencies = ['node_modules',
-        '.env',
-        '*.json',
-        '.idea',
-        '.git',
-        '.gitignore',
-        '*.iml',
-        '.*',
-        '*.md',
-        'LICENSE'];
+    nutIocContainer.useDependency({
+        IsInterceptor: true,
+        ServiceName: "errorInterceptor",
+        Namespace: "interceptors",
+        Service: ({ }) =>
+            (environment, next) => {
 
-    nutIocContainer.use({ dependencyPath: './', ignoredDependencies });
+                let result;
+                try {
+
+                    result = next(environment);
+
+                } catch (error) {
+
+                    appLogger.error(`ERROR: ${`${environment.moduleName}.${environment.method.name}`} method`, error);
+
+                    throw error;
+                }
+
+                return result;
+            }
+    });
+
+    nutIocContainer.useDependency({
+        IsInterceptor: true,
+        ServiceName: "logInterceptor",
+        Namespace: "interceptors",
+        Service: ({ }) =>
+            (environment, next) => {
+
+                const { method, args } = environment;
+
+                console.log(`ENTRY: ${method.name}(${JSON.stringify(args[0])}) function`)
+
+                const startDate = new Date();
+
+                const result = next(environment);
+
+                const elapsedMilliseconds = new Date() - startDate;
+
+                console.log(`SUCCESS: ${method.name} function returns //${result}: Elapsed milliseconds is ${elapsedMilliseconds}`);
+
+                return result;
+            }
+    });
 
     nutIocContainer.useDependency({
         ServiceName: "authorBasicInfo",
@@ -28,7 +61,39 @@ const mainAsync = async () => {
         Service: ({ authorBasicInfo }) => ({ ...authorBasicInfo, city: "London", mail: "kenanhancer@gmail.com" })
     });
 
-    const { greetingService, authorWithContacts } = await nutIocContainer.build();
+    nutIocContainer.useDependency({
+        ServiceName: "greetingHelper",
+        Service: ({ }) => ({
+            getFullName: ({ firstName, lastName }) => `${firstName} ${lastName}`
+        }),
+        Interceptor: ({ interceptors: { errorInterceptor, logInterceptor } }) => {
+
+            return [errorInterceptor, logInterceptor];
+        }
+    });
+
+    nutIocContainer.useDependency({
+        ServiceName: "greetingService",
+        Service: ({ greetingHelper: { getFullName } }) => ({
+            sayHello: ({ firstName, lastName }) => {
+
+                const fullName = getFullName({ firstName, lastName });
+
+                return `Hello ${fullName}`;
+            },
+            sayGoodbye: ({ firstName, lastName }) => {
+                const fullName = getFullName({ firstName, lastName });
+
+                return `Goodbye, ${fullName}`;
+            }
+        }),
+        Interceptor: ({ interceptors: { errorInterceptor, logInterceptor } }) => {
+
+            return [errorInterceptor, logInterceptor];
+        }
+    });
+
+    const { authorWithContacts, greetingService } = await nutIocContainer.build();
 
 
 
