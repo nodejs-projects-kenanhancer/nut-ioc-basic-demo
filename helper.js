0a635b39a77bcaef0cc077d5656d7ad647a3adb1
kenanhancer <kenanhancer@gmail.com>
2020-04-18 22:02:15 +0100

    unused files are deleted

--- helper.js
+++
@@ -1,7 +0,0 @@
-module.exports.ServiceName = ""; //fileName if empty,null or undefined
-
-module.exports.Service = ({
-    getFullName: ({ firstName, lastName }) => {
-        return `${firstName} ${lastName}`;
-    }
-});
