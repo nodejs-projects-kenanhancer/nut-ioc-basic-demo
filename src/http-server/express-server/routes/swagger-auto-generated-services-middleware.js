0c7ba6d3ab6f92c890d0e158a3a39eb7238a4d06
kenanhancer <kenanhancer@gmail.com>
2020-04-25 18:10:34 +0100

    nut-swagger version it updated to 1.0.4

--- src/http-server/express-server/routes/swagger-auto-generated-services-middleware.js
+++ src/http-server/express-server/routes/swagger-auto-generated-services-middleware.js
@@ -18,27 +18,12 @@ module.exports.Service = async ({ expressServer, swaggerDefinitions, defaultRequ
 
                 const { operationId } = methods[method];
 
-                const parts = operationId && operationId.split('.');
-
-                if (!parts || (parts && parts.length < 2)) {
-                    throw new Error(`${path} in Swagger definition doesn't have a proper operationId. Syntax should be like this "Greeting.sayHello"`)
-                }
-
-                const serviceId = parts[0];
-                const serviceModule = await dependencyProvider(serviceId);
-
-                if (!serviceModule) {
-                    console.error(`ERROR: Express Swagger Middleware couldn't find ${serviceId} dependency in Dependency Injection Framework.`);
-                    continue;
-                    // throw new Error(`${serviceId} dependency couldn't be find in Dependency Injection Framework.`);
-                }
-
-                const actionFunc = serviceModule[parts[1]];
+                const actionFunc = await dependencyProvider(operationId);
 
                 if (!actionFunc) {
-                    console.error(`ERROR: Express Swagger Middleware couldn't find ${parts[1]} function in ${serviceId} dependency.`);
+                    console.error(`ERROR: Express Swagger Middleware couldn't find ${servicoperationIdeId} dependency in Dependency Injection Framework.`);
                     continue;
-                    // throw new Error(`${parts[1]} function couldn't be find in ${serviceId} dependency.`);
+                    // throw new Error(`${serviceId} dependency couldn't be find in Dependency Injection Framework.`);
                 }
 
                 const expressPath = path.replace('}', '').replace('{', ':');
