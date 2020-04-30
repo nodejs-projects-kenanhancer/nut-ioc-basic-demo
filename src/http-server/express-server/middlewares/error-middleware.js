e10e78343f7734f6f9353e3418bfabb7e4760d70
kenanhancer <kenanhancer@gmail.com>
2020-04-30 01:46:50 +0100

    errorMiddleware is updated

--- src/http-server/express-server/middlewares/error-middleware.js
+++ src/http-server/express-server/middlewares/error-middleware.js
@@ -1,22 +1,20 @@
-module.exports.ServiceName = ""; //fileName if empty,null or undefined
-module.exports.Service = ({ }) =>
-    async (error, req, res, next) => {
+module.exports.ServiceName = ''; //fileName if empty,null or undefined
+module.exports.Service = ({}) => async (error, req, res, next) => {
+    // downstream error
+    if (error.message && error.message.error) {
+        error = error.message.error;
+    }
 
-        // downstream error
-        if (error.message && error.message.error) {
-            error = error.message.error;
-        }
+    if (typeof error['message'] !== 'string') {
+        error['message'] = '';
+    }
 
-        if (typeof error['message'] !== 'string') {
-            error['message'] = '';
-        }
+    const responseBody = error.message || 'ERROR';
+    res.statusCode = error.code;
 
-        const responseBody = "ERROR";
-        res.statusCode = error.response.status;
-
-        if ([400, 500].includes(res.statusCode)) {
-            res.send(responseBody);
-        } else {
-            res.send();
-        }
-    };
+    if ([400, 401, 403, 404, 405, 406, 412, 415, 500, 501].includes(res.statusCode)) {
+        res.send(responseBody);
+    } else {
+        res.send();
+    }
+};
